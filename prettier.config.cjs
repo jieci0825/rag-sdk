97cdad22031ef97e91ee7ef88e5f07c56830d497
coderjc <coderjc@qq.com>
2026-05-26 15:20:21 +0800

    feat: 添加 Prettier 配置文件以统一代码格式

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    // JavaScript、TypeScript 等文件统一使用单引号。
+    singleQuote: true,
+
+    // 尽量将每行控制在 120 个字符以内。
+    printWidth: 120,
+
+    // 多行对象、数组、导入等位置保留尾随逗号。
+    trailingComma: 'all',
+
+    // 每一级缩进使用 4 个空格。
+    tabWidth: 4,
+};
